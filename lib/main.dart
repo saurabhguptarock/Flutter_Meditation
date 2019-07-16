28f565445a4323292f001f9cffda070ec7c0da60
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-07-16 12:16:31 +0530

    Made full design

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,80 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Stack(
           children: <Widget>[
             Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(199, 183, 245, 1),
+                image: DecorationImage(
+                  image: AssetImage('assets/img/login_logo.png'),
+                ),
+              ),
               height: MediaQuery.of(context).size.height * .4,
-              color: Color.fromRGBO(199, 183, 245, 1),
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: EdgeInsets.only(top: 50, left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Meditation',
+                      style: TextStyle(
+                          color: Color.fromRGBO(33, 43, 70, 1),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                    ),
+                    Text(
+                      '3-10 MIN Course',
+                      style: TextStyle(
+                          color: Color.fromRGBO(70, 77, 108, 1),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * .8,
+                      child: Text(
+                        'Live happier and healthier by learning the fundamentals of meditation and  mindfulness',
+                        style: TextStyle(
+                            height: 1.2,
+                            color: Color.fromRGBO(70, 77, 108, 1),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 10,
+                        right: 180,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Colors.white,
+                            shape: BoxShape.rectangle),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 15),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  size: 35,
+                                ),
+                                labelText: "Search",
+                                fillColor: Colors.white,
+                                focusColor: Colors.white,
+                                border: InputBorder.none),
+                            keyboardType: TextInputType.text,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Positioned(
               top: MediaQuery.of(context).size.height * .4 - 40,
